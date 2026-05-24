918ae6efa8be89c8812d25f00afb6952e62e19e4
Elizabeth Conrad <lizcconrad@gmail.com>
2026-05-24 15:28:07 -0700

    add tracers to init

--- src/pogg_semantics/semantic_composition/__init__.py
+++ src/pogg_semantics/semantic_composition/__init__.py
@@ -1,5 +1,6 @@
 from pogg_semantics.semantic_composition._semantic_algebra import SemanticAlgebra
 from pogg_semantics.semantic_composition._semantic_composition import SemanticComposition
+from pogg_semantics.semantic_composition._call_tracer import SemAlgTracer, SemCompTracer
 from pogg_semantics.semantic_composition._sement_util import SEMENTUtil
 
 __all__ = [
